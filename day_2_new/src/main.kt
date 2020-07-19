5b5323de9ec29bc24e958854a7c01ebd7719dcfe
hatoriz <iam.nathdanai@gmail.com>
2020-07-19 11:47:27 +0700

    new

---
+++ day_2_new/src/main.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var x = 30
+
+    while(x>20){
+        x = x - 1
+        print("x is now $x")
+    }
+
+    for (i in 1..10){
+        x = x + 1
+    }
+    print("x is now $x")
+}
