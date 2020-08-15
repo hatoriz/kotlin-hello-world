a82bb56ba06a84ef4eabd085aaaf9acb90388e8f
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2020-08-16 00:02:05 +0700

    Completed game - scissor rock and paper

--- Hello_World_Kotlin/src/main.kt
+++ Hello_World_Kotlin/src/main.kt
@@ -2,6 +2,8 @@ fun main(args: Array<String>){
     val options = arrayOf("Rock", "Paper", "Scissors")
     val gameChoice = getGameChoice(options)
     val userChoice = getUserChoice(options)
+
+    printResult(userChoice, gameChoice)
 }
 
 fun getGameChoice(optionsParam: Array<String>) = optionsParam[(Math.random() * optionsParam.size).toInt()]
@@ -30,4 +32,25 @@ fun getUserChoice(optionsParam: Array<String>): String {
         if (!isValidChoice) println("You must enther a valid choice")
     }
     return userChoice
+}
+
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+
+    //Figure out the result
+    if (userChoice == gameChoice){
+        result = "Tie!"
+    }
+    else if ((userChoice == "Rock" && gameChoice == "Scissors") ||
+            (userChoice == "Paper" && gameChoice == "Rock") ||
+            (userChoice == "Scissors" && gameChoice == "Paper")){
+        result = "You win!"
+    }
+    else {
+        result = "You lose!"
+    }
+
+    //Print the result
+    println("You chose $userChoice. I chose $gameChoice. $result")
+
 }
