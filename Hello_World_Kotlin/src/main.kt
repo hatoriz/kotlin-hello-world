41d03382031cf0d8a5a170b28d92629f3398959a
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2020-08-15 23:31:50 +0700

    Clean main class

--- Hello_World_Kotlin/src/main.kt
+++ Hello_World_Kotlin/src/main.kt
@@ -1,11 +1,4 @@
-//Chapter 4
-//For 2 august 2020 class
-// Reformat Code from the main menu or press Ctrl+Alt+L
 fun main(){
 
-    val d = DrumKit(true, true)
-    d.playTopHat()
-    d.playSnare()
-    d.playTopHat()
 
 }
