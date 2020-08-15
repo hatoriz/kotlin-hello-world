a8c2a308fb500a77db63f9cb9e1d2bfcbaf865d9
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2020-08-15 22:37:39 +0700

    Refactor by moving all classes to animal.kt
    Refactor by moving all classes to animal.kt

--- Hello_World_Kotlin/src/Animal.kt
+++ Hello_World_Kotlin/src/Animal.kt
@@ -22,3 +22,38 @@ open class Animal {
     }
 
 }
+
+open class Canine : Animal() {
+    override fun roam() {
+        println("The Canin is roaming")
+    }
+
+}
+
+class Hippo : Animal() {
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+}
+
+class Wolf : Canine(){
+    override val image = "wolf.jpg"
+    override val food = "meat"
+    override val habitat = "forests"
+
+    override fun makeNoise() {
+        println("Hooooowl!")
+    }
+
+    override fun eat() {
+        println("The Wolf is eating $food")
+    }
+}
