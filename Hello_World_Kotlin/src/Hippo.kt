dda7160f001a0bece78434409b2ff4b7e141b39b
Hatoriz Kwansiripat <ninja.hatoriz@gmail.com>
2020-08-15 22:35:15 +0700

    Magent Code: Canine and Wolf
    Magent Code: Canine and Wolf

--- Hello_World_Kotlin/src/Hippo.kt
+++ Hello_World_Kotlin/src/Hippo.kt
@@ -11,3 +11,27 @@ class Hippo : Animal() {
         println("The Hippo is eating $food")
     }
 }
+
+//---------- Canine and Wolf ------------
+open class Canine : Animal() {
+    override fun roam() {
+        println("The Canin is roaming")
+    }
+
+}
+
+class Wolf : Canine(){
+    override val image = "wolf.jpg"
+    override val food = "meat"
+    override val habitat = "forests"
+
+    override fun makeNoise() {
+        println("Hooooowl!")
+    }
+
+    override fun eat() {
+        println("The Wolf is eating $food")
+    }
+}
+
+//---------- end of Canine and Wolf ------------
