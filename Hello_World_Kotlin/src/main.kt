32b2a91d672f56882facb87a4b8fc986b030b416
hatoriz <ninja.hatoriz@gmail.com>
2020-08-16 11:36:40 +0700

    สร้าง Superclass ชื่อ Animal

--- Hello_World_Kotlin/src/main.kt
+++ Hello_World_Kotlin/src/main.kt
@@ -1,4 +1,26 @@
 fun main(){
 
+}
 
+open class Animal{
+    open val image = ""
+    open val food = ""
+    open val habitat = ""
+    var hunger = 10
+
+    open fun makeNoise(){
+        println("This Animal is making noise!")
+    }
+
+    open fun eat(){
+        println("This Animal is eating!")
+    }
+
+    open fun roam(){
+        println("This Animal is roaming")
+    }
+
+    fun sleep(){
+        println("This Animal is sleeping")
+    }
 }
