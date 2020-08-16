aeec79ed7a102d1a3cc48b21ba1c044d56e1f6f6
hatoriz <ninja.hatoriz@gmail.com>
2020-08-16 12:18:44 +0700

    สร้าง override function ให้กับ Hippo กับ Cat

--- Hello_World_Kotlin/src/main.kt
+++ Hello_World_Kotlin/src/main.kt
@@ -1,6 +1,20 @@
 fun main(){
-    var hip = Hippo()
-    hip.eat()
+    //นี่คือ Hippo ของฉัน
+    var myhippo = Hippo()
+    myhippo.eat()
+    myhippo.roam()
+    myhippo.makeNoise()
+    myhippo.sleep()
+
+    println("")
+    
+    //สร้างแมวของเรา
+    var mycat = Cat()
+    //เขียนให้แมวเรา eat, roam, makenoise, sleep
+    mycat.eat()
+    mycat.roam()
+    mycat.makeNoise()
+    mycat.sleep()
 }
 
 open class Animal{
@@ -28,4 +42,30 @@ open class Animal{
 
 class Hippo : Animal() {
     //สร้างคลาส Hippo แต่ยังไม่ได้ทำอะไร
+    override val image = "hippo.jpg"
+    override val food = "grass"
+    override val habitat = "water"
+
+    override fun makeNoise() {
+        println("Grunt! Grunt!")
+    }
+
+    override fun eat() {
+        println("The Hippo is eating $food")
+    }
+
+}
+
+class Cat : Animal(){
+    override val image = "cat.jpg"
+    override val food = "อาหารแมว"
+    override val habitat = "บ้าน"
+
+    override fun makeNoise() {
+        println("Meow! Meow!")
+    }
+
+    override fun eat() {
+        println("The Cat is eating $food")
+    }
 }
