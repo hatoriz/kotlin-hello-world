56c2cdf7b273b11b34a5f1910e697d7377713ec2
hatoriz <ninja.hatoriz@gmail.com>
2020-08-16 10:53:22 +0700

    ลบไฟล์ที่ไม่ใช้ทิ้ง

--- Hello_World_Kotlin/src/Song.kt
+++
@@ -1,9 +0,0 @@
-class Song(val title: String, val artist: String) {
-    fun play(){
-        println("Playing the song ${title} by ${artist}")
-    }
-
-    fun stop(){
-        println("Stopped playing ${title}")
-    }
-}
